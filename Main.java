1cf39f6524ac13b75a579f79f768d7e5507817ff
Yashraj741 <yashrajkumawat8949@gmail.com>
2025-07-30 21:12:12 +0530

    file uploaded

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Java_stdin_stdout_1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        double pi = sc.nextDouble();
+        sc.nextLine();
+        String str = sc.nextLine();
+        
+        System.out.println("String: "+str);
+        System.out.println("Double: "+pi);
+        System.out.println("Int: "+num);
+    }
+}
