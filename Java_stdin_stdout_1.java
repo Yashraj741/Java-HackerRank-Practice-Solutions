e99f7479303271f26d310dc6223c6d79065c34fc
Yashraj741 <yashrajkumawat8949@gmail.com>
2025-07-30 21:09:08 +0530

    File namae changed

--- Java_stdin_stdout_1.java
+++ Java_stdin_stdout_1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Java_stdin_stdout {
+public class Java_stdin_stdout_1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
